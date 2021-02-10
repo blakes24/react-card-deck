87ab0bb15f573584f907ea634364da20c20d0fd7
blakes24 <blakes24@gmail.com>
2021-02-09 17:17:50 -0800

    Add auto draw

--- src/Deck.js
+++ src/Deck.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 const Deck = () => {
 	const [ deck, setDeck ] = useState(null);
 	const [ cards, setCards ] = useState([]);
+	const [ drawing, setDrawing ] = useState(false);
+	const intervalId = useRef();
 
 	useEffect(() => {
 		async function getDeck() {
@@ -14,19 +16,51 @@ const Deck = () => {
 		getDeck();
 	}, []);
 
-	async function getCard() {
-		const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/`);
-		const newCard = res.data.cards[0];
-		if (!newCard) {
-			return alert('Error: no cards remaining!');
-		}
-		setCards((cards) => [ ...cards, newCard ]);
-	}
+	useEffect(
+		() => {
+			async function getCard() {
+				try {
+					const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/`);
+					const newCard = res.data.cards[0];
+					if (!newCard) {
+						setDrawing(false);
+						throw new Error('no cards remaining!');
+					}
+					setCards((cards) => [ ...cards, newCard ]);
+				} catch (e) {
+					if (e.message === 'no cards remaining!') {
+						alert(e);
+					}
+				}
+			}
+			if (drawing) {
+				intervalId.current = setInterval(async () => {
+					await getCard();
+				}, 1000);
+			}
+			return () => {
+				clearInterval(intervalId.current);
+			};
+		},
+		[ drawing, setDrawing, deck ]
+	);
+
+	const startDrawing = () => {
+		setDrawing(true);
+	};
+
+	const stopDrawing = () => {
+		clearInterval(intervalId.current);
+		setDrawing(false);
+	};
 
 	return (
-		<div>
-			<button onClick={getCard}>Draw a card</button>
-			{cards.map((card) => <Card key={card.code} src={card.image} alt={`${card.value} of ${card.suit}`} />)}
+		<div className="Deck">
+			<button onClick={startDrawing}>Start Drawing</button>
+			<button onClick={stopDrawing}>Stop Drawing</button>
+			<div className="Deck-cards">
+				{cards.map((card) => <Card key={card.code} src={card.image} alt={`${card.value} of ${card.suit}`} />)}
+			</div>
 		</div>
 	);
 };
